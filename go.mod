c5d2b91e11fbda499957963ea82f15a7bf3474b2
Travis Cline <travis.cline@gmail.com>
2023-05-11 17:49:19 -0700

    slackdump: add cobra

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module github.com/tmc/slackdump
 
 go 1.20
 
-require github.com/slack-go/slack v0.12.2
+require (
+	github.com/slack-go/slack v0.12.2
+	github.com/spf13/cobra v1.7.0
+)
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"os"
-)
 
-var (
-	flagDCookie  = flag.String("d", "", "'d' cookie value")
-	flagDSCookie = flag.String("d-s", "", "'d-s' cookie value")
-	flagToken    = flag.String("token", "", "slack token")
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	flag.Parse()
-	ctx := context.Background()
-	if err := run(ctx, Options{
-		DCookie:  *flagDCookie,
-		DSCookie: *flagDSCookie,
-		Token:    *flagToken,
-	}); err != nil {
+	if err := RootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
+}
+
+func init() {
+	RootCmd.PersistentFlags().StringP("d-cookie", "d", "", "'d' cookie value")
+	RootCmd.PersistentFlags().StringP("ds-cookie", "s", "", "'d-s' cookie value")
+	RootCmd.PersistentFlags().StringP("token", "t", "", "slack token (see readme)")
+	RootCmd.MarkPersistentFlagRequired("d-cookie")
+	RootCmd.MarkPersistentFlagRequired("ds-cookie")
+	RootCmd.MarkPersistentFlagRequired("token")
+
+	RootCmd.AddCommand(ListConversationsCmd)
 }
 
 func run(ctx context.Context, opts Options) error {
@@ -32,4 +33,46 @@ func run(ctx context.Context, opts Options) error {
 	}
 	client.listConversations(ctx)
 	return nil
+}
+
+func newClientFromFlags(cmd *cobra.Command) (*slackClient, error) {
+	d, err := cmd.Flags().GetString("d-cookie")
+	if err != nil {
+		return nil, err
+	}
+	ds, err := cmd.Flags().GetString("ds-cookie")
+	if err != nil {
+		return nil, err
+	}
+	t, err := cmd.Flags().GetString("token")
+	if err != nil {
+		return nil, err
+	}
+	return newClient(t, d, ds)
+}
+
+var RootCmd = &cobra.Command{
+	Use:   "slackdump",
+	Short: "slackdump",
+	Long:  `slackdump`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.Usage()
+		return nil
+	},
+}
+
+var ListConversationsCmd = &cobra.Command{
+	Use:   "list-conversations",
+	Short: "list-conversations",
+	Long:  `list-conversations`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("listing")
+		ctx := context.Background()
+		c, err := newClientFromFlags(cmd)
+		if err != nil {
+			return err
+		}
+		c.listConversations(ctx)
+		return nil
+	},
 }
