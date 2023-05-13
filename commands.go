b6b904ce9be0866e77c45938fb1a404e2518057b
Travis Cline <travis.cline@gmail.com>
2023-05-12 18:07:51 -0700

    refactor to commands.go

---
+++ commands.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.PersistentFlags().StringP("d-cookie", "d", "", "'d' cookie value")
+	RootCmd.PersistentFlags().StringP("ds-cookie", "s", "", "'d-s' cookie value")
+	RootCmd.PersistentFlags().StringP("token", "t", "", "slack token (see readme)")
+	RootCmd.MarkPersistentFlagRequired("d-cookie")
+	RootCmd.MarkPersistentFlagRequired("ds-cookie")
+	RootCmd.MarkPersistentFlagRequired("token")
+
+	RootCmd.AddCommand(ListConversationsCmd)
+	RootCmd.AddCommand(ListUsersCmd)
+	RootCmd.AddCommand(ListDMsCmd)
+	RootCmd.AddCommand(DumpConversationCmd)
+	RootCmd.AddCommand(SendMessageCmd)
+}
+
+var RootCmd = &cobra.Command{
+	Use:   "slackdump",
+	Short: "slackdump",
+	Long:  `slackdump`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.Usage()
+		return nil
+	},
+}
+
+var ListConversationsCmd = &cobra.Command{
+	Use:   "list-conversations",
+	Short: "list-conversations",
+	Long:  `list-conversations`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+		c, err := newClientFromFlags(cmd)
+		if err != nil {
+			return err
+		}
+		c.listConversations(ctx)
+		return nil
+	},
+}
+
+var ListDMsCmd = &cobra.Command{
+	Use:   "list-dms",
+	Short: "list-dms",
+	Long:  `list-dms`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+		c, err := newClientFromFlags(cmd)
+		if err != nil {
+			return err
+		}
+		c.listConversations(ctx, "im")
+		return nil
+	},
+}
+
+var DumpConversationCmd = &cobra.Command{
+	Use:   "dump-conversation",
+	Short: "dump-conversation",
+	Long:  `dump-conversation`,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+		c, err := newClientFromFlags(cmd)
+		if err != nil {
+			return err
+		}
+		c.dumpConversation(ctx, args[0])
+		return nil
+	},
+}
+
+var ListUsersCmd = &cobra.Command{
+	Use:   "list-users",
+	Short: "list-users",
+	Long:  `list-users`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+		c, err := newClientFromFlags(cmd)
+		if err != nil {
+			return err
+		}
+		users, err := c.listUsers(ctx)
+		if err != nil {
+			return err
+		}
+		for _, u := range users {
+			j, _ := json.Marshal(u)
+			fmt.Println(string(j))
+		}
+		return nil
+	},
+}
+
+var SendMessageCmd = &cobra.Command{
+	Use:   "send-msg",
+	Short: "send-msg",
+	Long:  `send-msg`,
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+		c, err := newClientFromFlags(cmd)
+		if err != nil {
+			return err
+		}
+		return c.sendMessage(ctx, args[0], args[1])
+	},
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 
@@ -14,21 +13,6 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	RootCmd.PersistentFlags().StringP("d-cookie", "d", "", "'d' cookie value")
-	RootCmd.PersistentFlags().StringP("ds-cookie", "s", "", "'d-s' cookie value")
-	RootCmd.PersistentFlags().StringP("token", "t", "", "slack token (see readme)")
-	RootCmd.MarkPersistentFlagRequired("d-cookie")
-	RootCmd.MarkPersistentFlagRequired("ds-cookie")
-	RootCmd.MarkPersistentFlagRequired("token")
-
-	RootCmd.AddCommand(ListConversationsCmd)
-	RootCmd.AddCommand(ListUsersCmd)
-	RootCmd.AddCommand(ListDMsCmd)
-	RootCmd.AddCommand(DumpConversationCmd)
-	RootCmd.AddCommand(SendMessageCmd)
 }
 
 func run(ctx context.Context, opts Options) error {
@@ -54,97 +38,4 @@ func newClientFromFlags(cmd *cobra.Command) (*slackClient, error) {
 		return nil, err
 	}
 	return newClient(t, d, ds)
-}
-
-var RootCmd = &cobra.Command{
-	Use:   "slackdump",
-	Short: "slackdump",
-	Long:  `slackdump`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cmd.Usage()
-		return nil
-	},
-}
-
-var ListConversationsCmd = &cobra.Command{
-	Use:   "list-conversations",
-	Short: "list-conversations",
-	Long:  `list-conversations`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		c, err := newClientFromFlags(cmd)
-		if err != nil {
-			return err
-		}
-		c.listConversations(ctx)
-		return nil
-	},
-}
-
-var ListDMsCmd = &cobra.Command{
-	Use:   "list-dms",
-	Short: "list-dms",
-	Long:  `list-dms`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		c, err := newClientFromFlags(cmd)
-		if err != nil {
-			return err
-		}
-		c.listConversations(ctx, "im")
-		return nil
-	},
-}
-
-var DumpConversationCmd = &cobra.Command{
-	Use:   "dump-conversation",
-	Short: "dump-conversation",
-	Long:  `dump-conversation`,
-	Args:  cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		c, err := newClientFromFlags(cmd)
-		if err != nil {
-			return err
-		}
-		c.dumpConversation(ctx, args[0])
-		return nil
-	},
-}
-
-var ListUsersCmd = &cobra.Command{
-	Use:   "list-users",
-	Short: "list-users",
-	Long:  `list-users`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		c, err := newClientFromFlags(cmd)
-		if err != nil {
-			return err
-		}
-		users, err := c.listUsers(ctx)
-		if err != nil {
-			return err
-		}
-		for _, u := range users {
-			j, _ := json.Marshal(u)
-			fmt.Println(string(j))
-		}
-		return nil
-	},
-}
-
-var SendMessageCmd = &cobra.Command{
-	Use:   "send-msg",
-	Short: "send-msg",
-	Long:  `send-msg`,
-	Args:  cobra.ExactArgs(2),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		c, err := newClientFromFlags(cmd)
-		if err != nil {
-			return err
-		}
-		return c.sendMessage(ctx, args[0], args[1])
-	},
 }
